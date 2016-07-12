f36de8e77eafa84bba9dc432bf06119efb17b58d
Jayson Buquia <jayson.buquia@codeandtheory.com>
2016-07-12 20:10:56 +0800

    Create initial migrations and seeds

---
+++ database/migrations/2016_07_12_120400_create_expenses_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateExpensesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('expenses', function (Blueprint $table) {
+            $table->increments('id');
+            $table->decimal('amount',64,2);
+            $table->string('description');
+            $table->tinyInteger('occured');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('expenses');
+    }
+}
