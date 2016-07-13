5a98752ef5fcdf6833b856631b4ce94514025777
Jayson Buquia <jayson.buquia@codeandtheory.com>
2016-07-13 13:18:51 +0800

    Remove everything - will create custom framework from scratch

--- app/Templates/Default/Language/Da/messages.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return array (
-  'Dashboard' => 'Skrivebord',
-  'Forgot Password?' => '',
-  'Logout' => 'Log ud',
-  'Profile' => 'Bruger konto',
-  'User Login' => '',
-);
--- system/Session/TokenMismatchException.php
+++
@@ -1,5 +0,0 @@
-<?php 
-
-namespace Session;
-
-class TokenMismatchException extends \Exception {}
