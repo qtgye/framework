a07050d58ec33f81fa0729997f15d4ae7f257cdc
Jayson Buquia <jayson.buquia@codeandtheory.com>
2016-07-14 20:26:20 +0800

    Refine assertions

--- app/Commands/Test.php
+++ app/Commands/Test.php
@@ -1,13 +1,18 @@
 <?php namespace App\Commands;
 
+use App\Core\Config;
+use App\Core\Database;
+
 /**
 * 
 */
 class Test extends BaseCommand
 {	
-	function __construct($options)
+	private $failed_asserts = 0;
+
+	function __construct($arguments)
 	{
-		parent::__construct($options);
+		parent::__construct($arguments);
 	}
 
 
@@ -15,11 +20,21 @@ class Test extends BaseCommand
 	{
 		$method = $param ? $param : 'all';
 
+		$this->_echo("\r\n");
+
 		if ( method_exists($this, $method) ) {
-			call_user_func_array([$this,$method], [$this->options]);
+			call_user_func_array([$this,$method], [$this->arguments]);
 		} else {
 			$this->_echo("\033[31m\r\nInvalid test method.\r\n");
 		}
+
+		if ( $this->failed_asserts > 0 ) {
+			$this->_echo("\r\n\033[41;1;37m\r\n   {$this->failed_asserts} ASSERTION".($this->failed_asserts>1?'S':'')." FAILED!  ");
+		} else {
+			$this->_echo("\r\n\033[32m\r\n   ALL ASSERTIONS PASSED!");
+		}
+
+		$this->_echo("\r\n\r\n");
 	}
 
 
@@ -31,12 +46,13 @@ class Test extends BaseCommand
 	 */
 	private function assert( $assertion, $test = FALSE)
 	{
-		$this->_echo("\033[36m\r\n{$assertion}");
+		$this->_echo("\033[1;30m\r\n  {$assertion}");
 
-		if ( $test === NULL || $test === FALSE || empty($test) ) {
-			$this->_echo("\033[31m  ✕ FAIL\r\n\r\n");
+		if ( $test === NULL || $test === FALSE || empty($test) ) {			
+			$this->_echo("\033[31m  ✕ FAIL");
+			$this->failed_asserts++;
 		} else {
-			$this->_echo("\033[32m  ✓ PASS\r\n\r\n");
+			$this->_echo("\033[32m  ✓ PASS");
 		}
 	}
 
@@ -57,18 +73,28 @@ class Test extends BaseCommand
 				&& 	defined('ASSETS_DIR')
 				&& 	defined('PUBLIC_DIR');
 
-var_dump(ROOT_DIR);
-var_dump(APP_DIR);
-var_dump(ASSETS_DIR);
-var_dump(PUBLIC_DIR)		;
-
 		$this->assert('Config should set directory path constants',$paths);
 	}
 
 
 	private function db()
 	{
-		var_dump(\App\Core\Database::get_instance());
+		$file_exists = file_exists(ROOT_DIR.'/.config');
+		$this->assert('A config file should be existing',$file_exists);
+
+		if ( $file_exists ) {
+			$db_config = Config::get('DB_HOST')
+						&& Config::get('DB_USER')
+						&& Config::get('DB_PASSWORD')
+						&& Config::get('DB_NAME');
+			$this->assert('Should have basic database connection variables',$db_config);
+		}
+
+		if ( $db_config ) {
+			$this->assert('Should connect to database using correct settings',
+							Database::get_instance()->pdo instanceof \PDO);
+		}
+
 	}
 
 }
