0217d9c9f81ec937767064fb5ee1647004149495
Jayson Buquia <jayson.buquia@codeandtheory.com>
2016-07-14 19:54:16 +0800

    Add assertion

--- app/Commands/Test.php
+++ app/Commands/Test.php
@@ -13,7 +13,7 @@ class Test extends BaseCommand
 
 	public function run($param)
 	{
-		$method = $param;
+		$method = $param ? $param : 'all';
 
 		if ( method_exists($this, $method) ) {
 			call_user_func_array([$this,$method], [$this->options]);
@@ -22,5 +22,31 @@ class Test extends BaseCommand
 		}
 	}
 
+
+	/**
+	 * Assert function
+	 * @param  string $assertion Assertion message ('be able to fetch data')
+	 * @param  bool $test result of test
+	 * @return [type] [description]
+	 */
+	private function assert( $assertion, $test = FALSE)
+	{
+		$this->_echo("\033[36m\r\n{$assertion}");
+
+		if ( $test === NULL || $test === FALSE || empty($test) ) {
+			$this->_echo("\033[31m  ✕ FAIL\r\n\r\n");
+		} else {
+			$this->_echo("\033[32m  ✓ PASS\r\n\r\n");
+		}
+	}
+
+
+	/**
+	 * Runs all the tests
+	 */
+	private function all()
+	{
+		// $this->assert('Some Test Here...',TRUE);
+	}
 
 }
