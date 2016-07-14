3fd30ba71fa729d9d35c52970a896feec2930b56
Jayson Buquia <jayson.buquia@codeandtheory.com>
2016-07-14 17:52:24 +0800

    Add test command

---
+++ app/Commands/Test.php
@@ -0,0 +1,18 @@
+<?php namespace App\Commands;
+
+/**
+* 
+*/
+class Test extends BaseCommand
+{	
+	function __construct($options)
+	{
+		parent::__construct($options);
+	}
+
+
+	public function run($param)
+	{
+		// body...
+	}
+}
